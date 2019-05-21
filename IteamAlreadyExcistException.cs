896072e1fc62ff78fea8a6bbdd43cf5fccce8613
ShoshieCoder <46136018+ShoshieCoder@users.noreply.github.com>
2019-05-21 17:52:57 +0300

    Add files via upload

---
+++ IteamAlreadyExcistException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace mivhan
+{
+    [Serializable]
+    internal class IteamAlreadyExcistException : Exception
+    {
+        public IteamAlreadyExcistException()
+        {
+        }
+
+        public IteamAlreadyExcistException(string message) : base(message)
+        {
+        }
+
+        public IteamAlreadyExcistException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IteamAlreadyExcistException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ IteamNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace mivhan
+{
+    [Serializable]
+    internal class IteamNotFoundException : Exception
+    {
+        public IteamNotFoundException()
+        {
+        }
+
+        public IteamNotFoundException(string message) : base(message)
+        {
+        }
+
+        public IteamNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IteamNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
